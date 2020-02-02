a03ef5a5cd2f56613013a177e63d69685847b955
Daniel Bautista <DanielBautista@Daniels-MacBook-Pro.local>
2020-02-01 20:49:56 -0800

    Adding a file?

---
+++ classes.py
@@ -0,0 +1,5 @@
+# Here is a file where I will be testing how to use github
+# I have added the file to git so now it is being tracked
+print("hello world")
+
+# now I need to find a way to upload this file to git 
